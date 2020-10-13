3a4b5b76ee6f58488a8b995367ccec9b64ca4f01
roody95 <agbassahr@gmail.com>
2020-10-13 11:54:48 +0000

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { trigger, transition, query, style, animate, animateChild, group } from '@angular/animations';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
+  animations: [
+    trigger('outletAnimation', [
+      transition('* <=> *', [
+        style({ position: 'relative' }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%'
+          })
+        ]),
+        query(':enter', [
+          style({ left: '-100%' })
+        ]),
+        query(':leave', animateChild(), { optional: true }),
+        group([
+          query(':leave', [
+            animate('300ms ease-out', style({ left: '100%' }))
+          ], { optional: true }),
+          query(':enter', [
+            animate('300ms ease-out', style({ left: '0%' }))
+          ])
+        ]),
+        query(':enter', animateChild())
+      ]),
+    ])
+  ]
 })
 export class LayoutComponent implements OnInit {
+
+  side: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  preparedRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
+
+  getSide(event: any) {
+      this.side = event;
   }
 
 }
