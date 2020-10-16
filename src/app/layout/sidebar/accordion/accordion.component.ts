d4caa96b3b79a147cb1116f2d1a68b8a1c7571df
roody95 <agbassahr@gmail.com>
2020-10-16 10:37:46 +0000

    commit

--- src/app/layout/sidebar/accordion/accordion.component.ts
+++ src/app/layout/sidebar/accordion/accordion.component.ts
@@ -14,7 +14,6 @@ export class AccordionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.activeIds);
     $('#' + this.activeIds).collapse('toggle');
   }
 
