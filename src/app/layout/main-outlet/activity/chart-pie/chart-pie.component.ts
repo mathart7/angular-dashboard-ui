9e5d8ed48e653ec2ca5cff775dfcb98ea841265f
roody95 <agbassahr@gmail.com>
2020-10-16 03:10:16 +0000

    commit

--- src/app/layout/main-outlet/activity/chart-pie/chart-pie.component.ts
+++ src/app/layout/main-outlet/activity/chart-pie/chart-pie.component.ts
@@ -8,18 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ChartPieComponent implements OnInit {
 
 
-  //
+  // Pie chart config part
   public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
   public doughnutChartData = [120, 150, 180, 90];
   public doughnutChartType = 'doughnut';
 
-  //
+  // Pie chart config part
   public radarChartLabels = ['Q1', 'Q2', 'Q3', 'Q4'];
   public radarChartData = [
     {data: [120, 130, 180, 70], label: '2017'},
     {data: [90, 150, 200, 45], label: '2018'}
   ];
   public radarChartType = 'radar';
+
+  // Pie chart config part
+  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public pieChartData = [120, 150, 180, 90];
+  public pieChartType = 'pie';
 
   constructor() { }
 
