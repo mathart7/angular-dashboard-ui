b628c550b47fc09ae4b50ae40a6db512524c8ae1
roody95 <agbassahr@gmail.com>
2020-10-18 17:20:18 +0000

    commit

--- src/app/layout/main-outlet/tasks/tasks.component.ts
+++ src/app/layout/main-outlet/tasks/tasks.component.ts
@@ -8,7 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class TasksComponent implements OnInit {
 
   users = [
-
+    {
+      name: 'A R kuete',
+      avatar: 'https://gravatar.com/avatar/61f49a88d728ed09dc65432a651321bb?s=400&d=robohash&r=x',
+      grade: 'Chef de Projet'
+    },
+    {
+      name: 'Arline Baker',
+      avatar: 'https://gravatar.com/avatar/8c548df7644340531484f663b239e7a8?s=400&d=robohash&r=x',
+      grade: 'Vice chef'
+    },
+    {
+      name: 'Poole Walters',
+      avatar: 'https://gravatar.com/avatar/a02302b277f6aebff4402f9a3e838c5e?s=400&d=robohash&r=x',
+      grade: 'UI/UX Designer'
+    },
+    {
+      name: 'Myrtle Mcintyre',
+      avatar: 'https://gravatar.com/avatar/8f10e68333363f8c70afdf8d100547a1?s=400&d=robohash&r=x',
+      grade: 'Codeur'
+    },
+    {
+      name: 'Josie Mullen',
+      avatar: 'https://gravatar.com/avatar/a5b8a25ece063e93b1e0f1ce6db75094?s=400&d=robohash&r=x',
+      grade: 'Codeur'
+    },
+    {
+      name: 'Rosa Richards',
+      avatar: 'https://gravatar.com/avatar/8080b11e18f130aa523df28d55a648eb?s=400&d=robohash&r=x',
+      grade: 'Codeur'
+    },
+    {
+      name: 'Dyer Roy',
+      avatar: 'https://gravatar.com/avatar/59546b956cd1fcc1e55cb891e3cd3775?s=400&d=robohash&r=x',
+      grade: 'Analyste'
+    },
   ];
 
   teams = [
