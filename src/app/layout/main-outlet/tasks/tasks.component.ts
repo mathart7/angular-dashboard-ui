6bc4c97a0e046d029e0dc032ca56e23f7cabedde
roody95 <agbassahr@gmail.com>
2020-10-18 17:27:46 +0000

    commit

--- src/app/layout/main-outlet/tasks/tasks.component.ts
+++ src/app/layout/main-outlet/tasks/tasks.component.ts
@@ -46,7 +46,38 @@ export class TasksComponent implements OnInit {
   ];
 
   teams = [
+    {
+      name: 'team_project_1',
+      members: [
+        {
+          name: 'A R kuete',
+          avatar: 'https://gravatar.com/avatar/61f49a88d728ed09dc65432a651321bb?s=400&d=robohash&r=x',
+          grade: 'Chef de Projet'
+        },
+        {
+          name: 'Poole Walters',
+          avatar: 'https://gravatar.com/avatar/a02302b277f6aebff4402f9a3e838c5e?s=400&d=robohash&r=x',
+          grade: 'UI/UX Designer'
+        },
+        {
+          name: 'Josie Mullen',
+          avatar: 'https://gravatar.com/avatar/a5b8a25ece063e93b1e0f1ce6db75094?s=400&d=robohash&r=x',
+          grade: 'Codeur'
+        },
+        {
+          name: 'Rosa Richards',
+          avatar: 'https://gravatar.com/avatar/8080b11e18f130aa523df28d55a648eb?s=400&d=robohash&r=x',
+          grade: 'Codeur'
+        }
 
+      ],
+      todo: {
+        name: '',
+        details: '',
+        start: '',
+        end: ''
+      }
+    }
   ];
 
   tasks = [
